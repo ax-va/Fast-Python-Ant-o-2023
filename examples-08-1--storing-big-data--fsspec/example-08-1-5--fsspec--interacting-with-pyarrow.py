4876c02e665cb51340d4a148551a367bb8d9fc68
ax-va <vasiliev.informatik@t-online.de>
2024-05-05 15:23:47 +0200

    update

---
+++ examples-08-1--storing-big-data--fsspec/example-08-1-5--fsspec--interacting-with-pyarrow.py
@@ -0,0 +1,14 @@
+from fsspec.implementations.zip import ZipFileSystem
+from pyarrow import csv
+from pyarrow.fs import PyFileSystem, FSSpecHandler
+
+zfs = ZipFileSystem("dummy.zip")
+arrow_fs = PyFileSystem(FSSpecHandler(zfs))
+dummy1_csv = csv.read_csv(arrow_fs.open_input_stream("dummy/dummy1.csv"))
+print(dummy1_csv)
+# pyarrow.Table
+# col1: int64
+# col2: int64
+# ----
+# col1: [[1,3,5]]
+# col2: [[2,4,6]]
