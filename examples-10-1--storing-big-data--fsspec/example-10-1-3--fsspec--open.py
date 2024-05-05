b84b7d09d655d9fd5cb30a0508d859dc8b9431c4
ax-va <vasiliev.informatik@t-online.de>
2024-05-05 14:30:03 +0200

    update

---
+++ examples-10-1--storing-big-data--fsspec/example-10-1-3--fsspec--open.py
@@ -0,0 +1,29 @@
+import fsspec
+import pandas as pd
+
+with fsspec.open("dummy.txt", "r") as f:
+    for line in f:
+        print(repr(line))
+# 'Dummy text 1\n'
+# 'Dummy text 2'
+
+# Use URL chaining to open a csv file in a zip file
+with fsspec.open("zip://dummy/dummy1.csv::dummy.zip", "rt") as f:  # "zip://" is a protocol
+    print(f.read())
+# col1,col2
+# 1,2
+# 3,4
+# 5,6
+
+# URL chain to GitHub
+url_chain = (
+    "zip://dummy/dummy1.csv::github://ax-va:Fast-Python-Antao-2023@"
+    "/examples-10-1--storing-big-data--fsspec/dummy.zip"
+)
+
+with fsspec.open(url_chain) as f:
+    print(pd.read_csv(f))
+#    col1  col2
+# 0     1     2
+# 1     3     4
+# 2     5     6
