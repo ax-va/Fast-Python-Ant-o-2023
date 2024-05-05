eaf47e377ca63b680696be18a01892b3262c3ffa
ax-va <vasiliev.informatik@t-online.de>
2024-05-05 18:41:13 +0200

    update

--- examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
+++ examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
@@ -1,3 +1,4 @@
+import pyarrow as pa
 import pyarrow.parquet as pq
 from pyarrow import csv
 
@@ -13,13 +14,24 @@ large_data/yellow_tripdata_2020-01.csv.gz
 large_data/yellow_tripdata_2020-01.parquet
 """
 
-table = csv.read_csv("../large_data/yellow_tripdata_2020-01.csv.gz")
-pq.write_table(table, "../large_data/yellow_tripdata_2020-01.parquet")
+convert_options = csv.ConvertOptions(
+    column_types={
+        "VendorID": pa.uint8(),
+        "passenger_count": pa.uint8(),
+    }
+)
+
+table = csv.read_csv(
+    "../large_data/yellow_tripdata_2020-01.csv.gz",
+    convert_options=convert_options
+)
+
+pq.write_table(table, "../large_data/yellow_tripdata_2020-01.parquet", use_dictionary=False)
 
 import pandas as pd
 
 df = pd.read_parquet("../large_data/yellow_tripdata_2020-01.parquet")
-df.head()
+print(df.head())
 #    VendorID tpep_pickup_datetime tpep_dropoff_datetime  passenger_count  trip_distance  RatecodeID store_and_fwd_flag  ...  extra  mta_tax  tip_amount  tolls_amount  improvement_surcharge  total_amount  congestion_surcharge
 # 0       1.0  2020-01-01 00:28:15   2020-01-01 00:33:03              1.0            1.2         1.0                  N  ...    3.0      0.5        1.47           0.0                    0.3         11.27                   2.5
 # 1       1.0  2020-01-01 00:35:39   2020-01-01 00:43:04              1.0            1.2         1.0                  N  ...    3.0      0.5        1.50           0.0                    0.3         12.30                   2.5
