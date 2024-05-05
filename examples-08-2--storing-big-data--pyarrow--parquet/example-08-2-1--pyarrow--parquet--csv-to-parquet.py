861fae71f5f909a59a177af455a23053b4888843
ax-va <vasiliev.informatik@t-online.de>
2024-05-05 19:28:32 +0200

    update

--- examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
+++ examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
@@ -26,12 +26,12 @@ table = csv.read_csv(
     convert_options=convert_options
 )
 
-pq.write_table(table, "../large_data/yellow_tripdata_2020-01.parquet", use_dictionary=False)
+pq.write_table(table, "../large_data/yellow_tripdata_2020-01.parquet")
 
 import pandas as pd
 
 df = pd.read_parquet("../large_data/yellow_tripdata_2020-01.parquet")
-print(df.head())
+df.head()
 #    VendorID tpep_pickup_datetime tpep_dropoff_datetime  passenger_count  trip_distance  RatecodeID store_and_fwd_flag  ...  extra  mta_tax  tip_amount  tolls_amount  improvement_surcharge  total_amount  congestion_surcharge
 # 0       1.0  2020-01-01 00:28:15   2020-01-01 00:33:03              1.0            1.2         1.0                  N  ...    3.0      0.5        1.47           0.0                    0.3         11.27                   2.5
 # 1       1.0  2020-01-01 00:35:39   2020-01-01 00:43:04              1.0            1.2         1.0                  N  ...    3.0      0.5        1.50           0.0                    0.3         12.30                   2.5
