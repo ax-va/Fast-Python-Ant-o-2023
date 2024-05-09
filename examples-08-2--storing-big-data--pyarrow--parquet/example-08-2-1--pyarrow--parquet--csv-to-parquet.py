f105bc121d64a7a26dd66df879ea228a31fbe8ee
ax-va <vasiliev.informatik@t-online.de>
2024-05-09 21:02:39 +0200

    update

--- examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
+++ examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-1--pyarrow--parquet--csv-to-parquet.py
@@ -1,20 +1,24 @@
 """
 New York City taxi data
 """
+import pandas as pd
 import pyarrow as pa
+import pyarrow.compute as pc
 import pyarrow.parquet as pq
 from pyarrow import csv
 
 # Large data file is available under
 # https://tiago.org/yellow_tripdata_2020-01.csv.gz
 
-# Add big files to .gitignore with:
+# Add files to .gitignore with:
 """
-# large data for GitHub
+# large datasets for posting on GitHub
 # 109.9 MB
-large_data/yellow_tripdata_2020-01.csv.gz
+large_datasets/yellow_tripdata_2020-01.csv.gz
 # 113.1 MB
-large_data/yellow_tripdata_2020-01.parquet
+large_datasets/yellow_tripdata_2020-01.parquet
+# 86,4 MB
+large_datasets/yellow_tripdata_2020-01-ZSTD.parquet
 """
 
 convert_options = csv.ConvertOptions(
@@ -25,15 +29,18 @@ convert_options = csv.ConvertOptions(
 )
 
 table = csv.read_csv(
-    "../large_data/yellow_tripdata_2020-01.csv.gz",
+    "../large_datasets/yellow_tripdata_2020-01.csv.gz",
     convert_options=convert_options
 )
 
-pq.write_table(table, "../large_data/yellow_tripdata_2020-01.parquet")
+pq.write_table(table, "../large_datasets/yellow_tripdata_2020-01.parquet")
 
-import pandas as pd
+# Use the ZSTD compression on all columns
+pq.write_table(table, "../large_datasets/yellow_tripdata_2020-01-ZSTD.parquet", compression="ZSTD")
 
-df = pd.read_parquet("../large_data/yellow_tripdata_2020-01.parquet")
+# Read file into Pandas dataframe
+
+df = pd.read_parquet("../large_datasets/yellow_tripdata_2020-01.parquet")
 df.head()
 #    VendorID tpep_pickup_datetime tpep_dropoff_datetime  passenger_count  trip_distance  RatecodeID store_and_fwd_flag  ...  extra  mta_tax  tip_amount  tolls_amount  improvement_surcharge  total_amount  congestion_surcharge
 # 0       1.0  2020-01-01 00:28:15   2020-01-01 00:33:03              1.0            1.2         1.0                  N  ...    3.0      0.5        1.47           0.0                    0.3         11.27                   2.5
@@ -43,3 +50,18 @@ df.head()
 # 4       2.0  2020-01-01 00:01:58   2020-01-01 00:04:16              1.0            0.0         1.0                  N  ...    0.5      0.5        0.00           0.0                    0.3          4.80                   0.0
 #
 # [5 rows x 18 columns]
+
+# Create unordered and ordered datasets for demonstrating the efficiency of
+# the Run Length Encoding (RLE) encoding in the next example
+silly_table_unordered = table.from_arrays(
+    [table["VendorID"]],
+    ["unordered"],
+)
+
+silly_table_ordered = table.from_arrays(
+    [table["VendorID"].take(pc.sort_indices(table["VendorID"]))],
+    ["ordered"],
+)
+
+pq.write_table(silly_table_unordered, "../datasets/silly_unordered.parquet")  # 953.266 Bytes
+pq.write_table(silly_table_ordered, "../datasets/silly_ordered.parquet")  # 1.976 Bytes
--- examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-2--pyarrow--parquet--metadata--schema--row_group.py
+++ examples-08-2--storing-big-data--pyarrow--parquet/example-08-2-2--pyarrow--parquet--metadata--schema--row_group.py
@@ -1,20 +1,16 @@
 import pyarrow.parquet as pq
-parquet_file = pq.ParquetFile("../large_data/yellow_tripdata_2020-01.parquet")
+
+parquet_file = pq.ParquetFile("../large_datasets/yellow_tripdata_2020-01.parquet")
 
 print("parquet_file.metadata:\n", parquet_file.metadata)
 # parquet_file.metadata:
 #  <pyarrow._parquet.FileMetaData object at 0x78001ed43420>
 #   created_by: parquet-cpp-arrow version 16.0.0
-#   num_columns: 18
-#   num_rows: 6405008
-#   num_row_groups: 7
+#   num_columns: 18  # <-- There are 18 columns.
+#   num_rows: 6405008  # <-- There are 6,405,008 rows.
+#   num_row_groups: 7  # <-- There is 7 row groups in the file that is a partition of the total of rows.
 #   format_version: 2.6
 #   serialized_size: 14846
-
-# Notice:
-# There are 18 columns.
-# There are 6,405,008 rows.
-# There is 7 row groups in the file that is a partition of the total of rows.
 
 print("parquet_file.schema:\n", parquet_file.schema)
 # parquet_file.schema:
@@ -26,7 +22,7 @@ print("parquet_file.schema:\n", parquet_file.schema)
 #   optional int32 field_id=-1 passenger_count (Int(bitWidth=8, isSigned=false));
 #   optional double field_id=-1 trip_distance;
 #   optional int64 field_id=-1 RatecodeID;
-#   optional binary field_id=-1 store_and_fwd_flag (String);
+#   optional binary field_id=-1 store_and_fwd_flag (String);  # <-- Text is stored as general binary data
 #   optional int64 field_id=-1 PULocationID;
 #   optional int64 field_id=-1 DOLocationID;
 #   optional int64 field_id=-1 payment_type;
@@ -47,3 +43,124 @@ print("parquet_file.metadata.row_group(0):\n", parquet_file.metadata.row_group(0
 #   num_rows: 1048576
 #   total_byte_size: 139470304
 #   sorting_columns: ()
+
+# Look at the existing metadata for columns
+
+tip_amount_col_meta = parquet_file.metadata.row_group(0).column(13)
+print("tip_amount_col_meta:\n", tip_amount_col_meta)
+# tip_amount_col_meta:
+#  <pyarrow._parquet.ColumnChunkMetaData object at 0x725c5ffcf9c0>
+#   file_offset: 16754437
+#   file_path:
+#   physical_type: DOUBLE
+#   num_values: 1048576
+#   path_in_schema: tip_amount
+#   is_stats_set: True
+#   statistics:  # <-- The statistical information started here
+#     <pyarrow._parquet.Statistics object at 0x725c5ffcfa60>
+#       has_min_max: True
+#       min: -7.0
+#       max: 450.0
+#       null_count: 0
+#       distinct_count: None
+#       num_values: 1048576
+#       physical_type: DOUBLE
+#       logical_type: None
+#       converted_type (legacy): NONE
+#   compression: SNAPPY  # <-- The compression type (algorithm) can be different for different columns or cannot be used at all.
+#   encodings: ('PLAIN', 'RLE', 'RLE_DICTIONARY')
+#   has_dictionary_page: True
+#   dictionary_page_offset: 15351114
+#   data_page_offset: 15360558
+#   total_compressed_size: 1403323
+#   total_uncompressed_size: 1504833
+
+# Compression algorithms:
+# https://facebook.github.io/zstd/#benchmarks
+
+table = parquet_file.read()
+
+len(table["tip_amount"].unique())
+# 3626
+
+# encoding a column with a dictionary
+# -> long values are converted to an indirect reference
+# -> only 3626 different values with the 64-bits double type
+# -> encoding can reduce from 64 bits to 12 bits per value
+# -> enough to encode up to 4096 values
+
+# Demonstrate Run Length Encoding (RLE) encoding
+
+silly_unordered = pq.ParquetFile("../datasets/silly_unordered.parquet")
+# <pyarrow.parquet.core.ParquetFile at 0x72768c7cf350>
+
+silly_unordered_group = silly_unordered.metadata.row_group(0)
+# <pyarrow._parquet.RowGroupMetaData object at 0x72768e4e1d50>
+#   num_columns: 1
+#   num_rows: 1048576
+#   total_byte_size: 157418
+#   sorting_columns: ()
+
+print("silly_unordered_group.column(0):\n", silly_unordered_group.column(0))
+# silly_unordered_group.column(0):
+#  <pyarrow._parquet.ColumnChunkMetaData object at 0x72768db4d030>
+#   file_offset: 156686
+#   file_path:
+#   physical_type: INT32
+#   num_values: 1048576
+#   path_in_schema: unordered
+#   is_stats_set: True
+#   statistics:
+#     <pyarrow._parquet.Statistics object at 0x72768db4ddf0>
+#       has_min_max: True
+#       min: 1
+#       max: 2
+#       null_count: 0
+#       distinct_count: None
+#       num_values: 1048576
+#       physical_type: INT32
+#       logical_type: Int(bitWidth=8, isSigned=false)
+#       converted_type (legacy): UINT_8
+#   compression: SNAPPY
+#   encodings: ('PLAIN', 'RLE', 'RLE_DICTIONARY')
+#   has_dictionary_page: True
+#   dictionary_page_offset: 4
+#   data_page_offset: 28
+#   total_compressed_size: 156682
+#   total_uncompressed_size: 157418
+
+silly_ordered = pq.ParquetFile("../datasets/silly_ordered.parquet")
+# <pyarrow.parquet.core.ParquetFile at 0x72766bb3ae50>
+
+silly_ordered_group = silly_ordered.metadata.row_group(0)
+# <pyarrow._parquet.RowGroupMetaData object at 0x72767dd2a4d0>
+#   num_columns: 1
+#   num_rows: 1048576
+#   total_byte_size: 69
+#   sorting_columns: ()
+
+print("silly_ordered_group.column(0):\n", silly_ordered_group.column(0))
+# silly_ordered_group.column(0):
+#  <pyarrow._parquet.ColumnChunkMetaData object at 0x72766bba7b00>
+#   file_offset: 77
+#   file_path:
+#   physical_type: INT32
+#   num_values: 1048576
+#   path_in_schema: ordered
+#   is_stats_set: True
+#   statistics:
+#     <pyarrow._parquet.Statistics object at 0x72768db284f0>
+#       has_min_max: True
+#       min: 1
+#       max: 1
+#       null_count: 0
+#       distinct_count: None
+#       num_values: 1048576
+#       physical_type: INT32
+#       logical_type: Int(bitWidth=8, isSigned=false)
+#       converted_type (legacy): UINT_8
+#   compression: SNAPPY
+#   encodings: ('PLAIN', 'RLE', 'RLE_DICTIONARY')
+#   has_dictionary_page: True
+
+# -> RLE typically works well for ordered fields or fields with few values
